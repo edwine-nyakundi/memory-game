89306670bb2bae24609cb6e91999fba4feae90bd
edwine-nyakundi <edwinenyakundi@gmail.com>
2024-07-28 21:46:49 +0300

    first commit

---
+++ src/app/ranking-form/ranking-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ranking-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './ranking-form.component.html',
+  styleUrl: './ranking-form.component.css'
+})
+export class RankingFormComponent {
+
+}
---
+++ src/app/ranking-list/ranking-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ranking-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './ranking-list.component.html',
+  styleUrl: './ranking-list.component.css'
+})
+export class RankingListComponent {
+
+}
